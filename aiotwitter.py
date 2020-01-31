9305c6b721340d7faf430a9d0415f21390ea1351
Static Files <staticfiles@bard.dreamhost.com>
2020-01-30 19:04:09 -0800

    Got it working!

--- aiotwitter.py
+++ aiotwitter.py
@@ -1,10 +1,14 @@
 """asyncio TWitter library using HTTPX"""
+import base64
+import hashlib
+import hmac
 import logging
 import os
 import random
 import string
 import sys
 import time
+from urllib.parse import urlparse, quote, urlencode
 
 import httpx
 
@@ -12,7 +16,17 @@ BASE_URL = "https://api.twitter.com/1.1"
 
 logger = logging.getLogger(__name__)
 
-def _get_base_url(self, full_url):
+def _create_parameter_string(parameters):
+    parameter_string = ""
+    for k, v in sorted(parameters.items()):
+        if len(str(v)) > 0:
+            parameter_string += "%s=%s&" % (quote(k), quote(str(v), ""))
+        
+    parameter_string = parameter_string[:-1]
+
+    return parameter_string
+
+def _get_base_url(full_url):
     """returns a URL without query string parameters"""
     parse = urlparse(full_url)
     return "%s://%s%s" % (parse.scheme, parse.netloc, parse.path)
@@ -25,11 +39,11 @@ def _create_signature(parameters, http_method, resource_url):
     base_url = _get_base_url(resource_url)
     signature_base_string = "%s&%s&%s" % (http_method.upper(), quote(base_url, ""), quote(parameter_string, ""))
 
-    signing_key = self.consumer_secret + "&" + os.environ['TWITTER_OAUTH_TOEKN_SECRET']
+    signing_key = os.environ['TWITTER_CONSUMER_SECRET'] + "&" + os.environ['TWITTER_OAUTH_TOKEN_SECRET']
 
-    return base64.b64encode(hmac.new(signing_key, signature_base_string, hashlib.sha1).digest())
+    return base64.b64encode(hmac.new(signing_key.encode(), signature_base_string.encode(), hashlib.sha1).digest())
 
-def _create_header_string(params: dict):
+def _create_header_string(params: dict, resource_url):
     """
     https://developer.twitter.com/en/docs/basics/authentication/oauth-1-0a/authorizing-a-request
     """
@@ -50,11 +64,12 @@ def _create_header_string(params: dict):
     for k, v in params.items():
         parameters[k] = v
 
-    parameters["oauth_signature"] = _create_signature(parameters)
+    parameters["oauth_signature"] = _create_signature(parameters, http_method='GET', resource_url=resource_url)
 
     header_string = "OAuth "
     for k, v in sorted(parameters.items()):
-        if k[:6] == "oauth_" and len(v) > 0 and "callback" not in v:
+        print(k[:6])
+        if k[:6] == "oauth_" and len(str(v)) > 0 and "callback" not in str(v):
             header_string += '%s="%s", ' % (quote(k), quote(v, ""))
 
     # get rid of last comma & space
@@ -63,8 +78,9 @@ def _create_header_string(params: dict):
 
 
 async def _call(client: httpx.AsyncClient, endpoint: str, **params):
+    resource_url = f'{BASE_URL}/{endpoint}'
     headers = {
-        "Authorization": _create_header_string(params),
+        "Authorization": _create_header_string(params, resource_url),
     }
     resp = await client.get(f"{BASE_URL}/{endpoint}", params=params, headers=headers)
     logger.debug("got response from %s", str(dir(resp)))
